1ed5b61d1bed6e7ed2576f1324633e4bf6b1f073
vewong <medras13@gmail.com>
2014-09-24 18:24:22 -0700

    Project Start
    Basically the basic Unity project, quick raccoon reproduction, and my
    code restructuring for today.

---
+++ Assets/Scripts/MissionController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class MissionController : MonoBehaviour
+{
+    public delegate void BreedEvent(Raccoon parent, Raccoon[] babies);
+    public static BreedEvent breedEventHandler;
+
+    public Text raccoonCountDisplay;
+
+    //each entry of this array will be the count of that type of raccoon
+    //should try to set up an enum later
+    int[] raccoons = new int[5];
+    List<int> pairOfRaccoons = new List<int>();
+
+    //amount of time that should pass before a new raccoon is created
+    float breedingTime, breedingTimeMin = 3f, breedingTimeMax = 5f;
+
+    //font animation
+    int displayNum;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < raccoons.Length; i++)
+        {
+            //set count of each raccoon type to 0
+            raccoons[i] = 0;
+        }
+
+        //PROTOTYPE:
+        raccoons[0] = 2;
+        breedingTime = Random.Range(breedingTimeMin, breedingTimeMax);
+        displayNum = raccoons[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (breedingTime <= 0)
+        {
+            startBreeding();
+        }
+
+        breedingTime -= Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+
+        //hopefully slowly tick up the racoon count instead of it just appearing...
+        if (displayNum < raccoons[0])
+        {
+            displayNum++;
+        }
+        else
+        {
+            displayNum = raccoons[0];
+        }
+
+        //GUI.Label(new Rect(10, 0, 100, 50), "Raccoons: " + displayNum);
+        raccoonCountDisplay.text = "Raccoons: " + displayNum;
+    }
+
+    /// <summary>
+    /// Breed raccoons!
+    /// </summary>
+    void startBreeding()
+    {
+        //first off, let's reset breedingTime
+        breedingTime = Random.Range(breedingTimeMin, breedingTimeMax);
+
+        //check which (if any) types of raccoons have breeding pairs (aka more than 2)
+        for (int i = 5; i < raccoons.Length; i++)
+        {
+            if (raccoons[i] > 2)
+            {
+                //should we be tracking how many breeding pairs there are in each type of raccoon or just if there is a breeding pair?
+                //if so, I need to change this to a 2d array or just an array of ints..
+                pairOfRaccoons.Add(i);
+            }
+        }
+
+        //need to pick a new raccoon type to make based on which raccoon types have breeding pairs
+        int raccoonType = Random.Range(0, pairOfRaccoons.Count);
+        //now, a random amount of offspring to make
+        //maybe each raccoon type has a certain number of offspring to pull from but probably not
+        //more likely they each have different breeding times?
+        int newRaccoons = Random.Range(1, 5);
+
+        //add these to the right raccoon type
+        raccoons[raccoonType] += newRaccoons;
+    }
+}
---
+++ Assets/Scripts/Raccoon.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raccoon : MonoBehaviour {
+
+    string type;
+    float minReproTime, maxReproTime;
+    int minReproRate, maxReproRate;
+
+	// constructor
+	void Raccoon(string myBreed) 
+    {
+        type = myBreed;
+        minReproTime = 5f;
+        maxReproTime = 7f;
+        minReproRate = 3;
+        maxReproTime = 5;
+	}
+
+    void Raccoon(string myBreed, float minTime, float maxTime, int minOffspring, int maxOffspring)
+    {
+        type = myBreed;
+        minReproTime = minTime;
+        maxReproTime = maxTime;
+        minReproRate = minOffspring;
+        maxReproRate = maxOffspring;
+    }
+	
+	//getters
+    protected string Type()
+    {
+        return type;
+    }
+
+    //setters
+    //pretty much going to have to use these when players get powerups
+    protected void ChangeBreedingTime(float minTime, float maxTime)
+    {
+        minReproTime = minTime;
+        maxReproTime = maxTime;
+    }
+
+    protected void ChangeOffsprings(int minOffspring, int maxOffspring)
+    {
+        minReproRate = minOffspring;
+        maxReproRate = maxOffspring;
+    }
+
+    //other methods
+    //do raccoons keep track of when they should breed?
+
+    //make more raccoons!
+    protected int Multiply()
+    {
+        return Random.Range(minReproRate, maxReproRate);
+    }
+}
